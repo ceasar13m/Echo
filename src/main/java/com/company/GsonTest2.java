6adb7dc6018bb4b0b37a9cdac8903b1f0945eaeb
ceasar13m <muharlyamovy@gmail.com>
2019-02-09 22:33:20 +0300

    13

--- src/main/java/com/company/GsonTest2.java
+++ src/main/java/com/company/GsonTest2.java
@@ -9,11 +9,15 @@ import java.io.FileReader;
 
 public class GsonTest2 {
     public static void main(String[] args) throws FileNotFoundException {
+
         String filename = "C:\\Users\\muhar\\IdeaProjects\\Echo\\src\\main\\java\\com\\company\\clientName.json";
         FileReader fileReader = new FileReader(filename);
+
         Gson gson = new Gson();
-        Teachers teachers = gson.fromJson(fileReader ,Teachers.class);
-        for(Teacher teacher : teachers.teachers){
+
+        Teachers teachers = gson.fromJson(fileReader, Teachers.class);
+
+        for (Teacher teacher : teachers.teachers) {
             System.out.println(teacher);
         }
     }
--- src/main/java/com/company/model/Teacher.java
+++ src/main/java/com/company/model/Teacher.java
@@ -1,25 +1,17 @@
 package com.company.model;
 
-import java.util.ArrayList;
 
 public class Teacher {
 
 
-    public String name;
+    public String login;
 
-    public int exp;
+    public String password;
 
-    public ArrayList<String> subject;
 
     @Override
     public String toString() {
-
-        String subj = "";
-        for (int i = 0; i < subject.size(); i++) {
-            subj = subj + ", " + subject.get(i);
-        }
-
-        return name + " " + exp + " " + subj;
+        return login + " " + password;
     }
 
 }
